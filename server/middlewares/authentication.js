f17c1a2f65ad58001ab1b02cc1328b260f2ec22b
Jhano <alejanoo.f@gmail.com>
2020-09-23 03:58:28 -0300

    Login con autentificacion de token y roles

---
+++ server/middlewares/authentication.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+
+let verificaToken = (req, res, next) => {
+    let token = req.get("token");
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err
+            })
+        }
+        req.usuario = decoded.Usuario;
+        next();
+    })
+
+
+}
+
+let verificaAdminRole = (req, res, next) => {
+    let usuario = req.usuario;
+
+    if (usuario.role === "ADMIN_ROLE") {
+        next();
+    } else {
+        return res.json({
+            ok: false,
+            message: "Rol sin autorización"
+        })
+    }
+}
+
+module.exports = {
+    verificaToken,
+    verificaAdminRole
+}
